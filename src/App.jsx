211c9315fd9f8ac356bf847c86730e71e133dc16
jessicahylee <jehyerinn@gmail.com>
2023-11-14 12:32:51 -0500

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from './components/footer'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/home'
 import Form from './components/form'
-
+import Profile from './pages/profile'
 import './styles/global.scss'
 
 function App() {
@@ -12,7 +12,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="form" element={<Form/>} />
+        <Route path="form" element={<Form />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/profile.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './profile.scss'
+
+// const SERVER_URL = process.env.REACT_APP_SERVER_URL
+
+const Profile = () => {
+  const [isAuthenticating, setIsAuthenticating] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [profileData, setProfileData] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/auth/profile`, { withCredentials: true })
+      .then((res) => {
+        setIsAuthenticating(false)
+        setIsLoggedIn(true)
+        setProfileData(res.data)
+      })
+      .catch((err) => {
+        // If we are getting back 401 (Unauthorized) back from the server, means we need to log in
+        if (err.response.status === 401) {
+          // Update the state: done authenticating, user is not logged in
+          setIsAuthenticating(false)
+          setIsLoggedIn(false)
+        } else {
+          console.log('Error authenticating', err)
+        }
+      })
+  }, [])
+
+  const formatDate = (date) => {
+    // Return date formatted as 'month/day/year'
+    return new Date(date).toLocaleDateString('en-US')
+  }
+
+  // While the component is authenticating, do not render anything (alternatively, this can be a preloader)
+  if (isAuthenticating) return null
+
+  return (
+    <section className="profile-page">
+      <h1>Profile Page</h1>
+      {/* If user is logged in, render their profile information */}
+      {isLoggedIn ? (
+        profileData && (
+          <>
+            <h2>Hello, {profileData.username}</h2>
+            <h3>Registered since: {formatDate(profileData.updated_at)}</h3>
+            <img
+              className="profile-page__avatar"
+              src={profileData.avatar_url}
+              alt={`${profileData.username} avatar`}
+            />
+            <div className="profile-page__logout-wrapper">
+              {/* Render a logout button */}
+            </div>
+          </>
+        )
+      ) : (
+        // If user is not logged in, render a login button
+        <>
+          <p>
+            <strong>This page requires authentication.</strong>
+          </p>
+        </>
+      )}
+    </section>
+  )
+}
+
+export default Profile
