4becc94159c38d9148d264272e1c592319d96a82
jessicahylee <jehyerinn@gmail.com>
2023-12-13 17:19:02 -0500

    Crypto list update

--- src/helpers/ListofCoins.jsx
+++ src/helpers/ListofCoins.jsx
@@ -3,14 +3,16 @@ import Bitcoin from '../assets/Bitcoin-Logo.png'
 import Dogecoin from '../assets/dogecoin_symbol.jpeg'
 import Litecoin from '../assets/litecoin_symbol.webp'
 import Ripple from '../assets/ripple_symbol.jpeg'
+import Solana from '../assets/Solana_logo.png'
+import Cardano from '../assets/she-trades-cardano.avif'
 
 export const ListofCoins = [
   {
     name: 'Ethereum',
-    price: '$2,667.31',
+    price: '$2.252.98',
     image: Ethereum,
     description:
-      'Ethereum is a decentralized blockchain with smart contract functionality. Ether is the native cryptocurrency of the platform. Among cryptocurrencies, ether is second only to bitcoin in market capitalization. It is open-source software. Ethereum was conceived in 2013 by programmer Vitali',
+      'Both a cryptocurrency and a blockchain platform, Ethereum is a favorite of program developers because of its potential applications, like so-called smart contracts that automatically execute when conditions are met and like non-fungible tokens.',
   },
   {
     name: 'Bitcoin',
@@ -24,7 +26,7 @@ export const ListofCoins = [
     price: '$0.11',
     image: Dogecoin,
     description:
-      'Dogecoin.com promotes the currency as the "fun and friendly Internet currency", referencing its origins as a "joke".[9] Software engineers Billy Markus and Jackson Palmer launched the satirical cryptocurrency as a way to make fun of Bitcoin and the many other cryptocurrencies boasting grand plans to take over the world',
+      'Dogecoin was famously started as a joke in 2013 but rapidly evolved into a prominent cryptocurrency thanks to a dedicated community and creative memes. Unlike many other cryptos, there is no limit on the number of Dogecoins that can be created, which leaves the currency susceptible to devaluation as supply increases.',
   },
   {
     name: 'Litecoin',
@@ -39,5 +41,19 @@ export const ListofCoins = [
     image: Ripple,
     description:
       'In December 2020, Ripple Labs and two of its executives were sued by the  (SEC) for selling XRP tokens, which the SEC classified as unregistered securities. In July 2023, the court ruled that XRP, as a digital token, is not in and of itself a contract, transaction, or scheme that embodies the Howey requirements of an investment contract.',
+  },
+  {
+    name: 'Solana',
+    price: '$71.44',
+    image: Solana,
+    description:
+      'Solana is a highly functional open source project that banks on blockchain technology’s permissionless nature to provide decentralized finance (DeFi) solutions. While the idea and initial work on the project began in 2017, Solana was officially launched in March 2020 by the Solana Foundation with headquarters in Geneva, Switzerland.',
+  },
+  {
+    name: 'Cardano',
+    price: '$0.6484',
+    image: Cardano,
+    description:
+      'Cardano was founded back in 2017, and named after the 16th century Italian polymath Gerolamo Cardano. The native ADA token takes its name from the 19th century mathematician Ada Lovelace, widely regarded as the world’s first computer programmer. The ADA token is designed to ensure that owners can participate in the operation of the network. Because of this, those who hold the cryptocurrency have the right to vote on any proposed changes to the software.',
   },
 ]
